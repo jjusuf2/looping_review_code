477512b31c4c37e67796fff0d00d3ebcded3eb72
jjusuf2 <jjusuf@mit.edu>
2026-03-26 16:29:51 -0400

    fixing bug in CTCF search time code

--- analysis_code/Fig3_get_CTCF_search_times_actual.py
+++ analysis_code/Fig3_get_CTCF_search_times_actual.py
@@ -11,26 +11,24 @@ from utils import *  # key functions for this project
 
 THRESHOLD = 50
 
-dt_arr = 0.2
+dt = 0.2
 loop_num_arr = np.array([0, 1, 2])
 noise = 0
 n_reps = 10
 
-for dt in dt_arr:
-    print(f'working on dt={dt}', flush=True)
-    total_iters = len(loop_num_arr) * n_reps
-    with tqdm(total=total_iters, desc="calculating search times", unit="rep") as pbar:
-        rows_CTCF = []
-        for loop_num in loop_num_arr:
-            search_times_arr = search_times_CTCF_all_reps(loop_num, noise, non_sticky=False, threshold=THRESHOLD,
-                                                            target_frame_duration=dt, pbar=pbar)
-            for t_search in search_times_arr:
-                rows_CTCF.append(
-                    {
-                        "loop": loop_num,
-                        "time": t_search,
-                    }
-                )
+total_iters = len(loop_num_arr) * n_reps
+with tqdm(total=total_iters, desc="calculating search times", unit="rep") as pbar:
+    rows_CTCF = []
+    for loop_num in loop_num_arr:
+        search_times_arr = search_times_CTCF_all_reps(loop_num, noise, non_sticky=False, threshold=THRESHOLD,
+                                                        target_frame_duration=dt, pbar=pbar)
+        for t_search in search_times_arr:
+            rows_CTCF.append(
+                {
+                    "loop": loop_num,
+                    "time": t_search,
+                }
+            )
 
-    search_times_CTCF = pd.DataFrame(rows_CTCF)
-    search_times_CTCF.to_csv(f'../data/search_times_CTCF_actual.csv', index=False, float_format="%.2f")
+search_times_CTCF = pd.DataFrame(rows_CTCF)
+search_times_CTCF.to_csv(f'../data/search_times_CTCF_actual.csv', index=False, float_format="%.2f")
